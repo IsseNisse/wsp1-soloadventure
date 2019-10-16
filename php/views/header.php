a566a6f77de68eeefd779477d0c76d23fb2bf8c9
IsseNisse <Isakhafiu2001>
2019-10-16 11:18:35 +0200

    nice

---
+++ php/views/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar">
+    <div class="container">
+        <a class="navbar-brand" href="index.php"><img id="logo" src="img/scary.png" alt="Logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../wsp1-tweet/">Tweet</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Solo adventure</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
